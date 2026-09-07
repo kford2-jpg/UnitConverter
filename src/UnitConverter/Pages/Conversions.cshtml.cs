550dea37d5f1e9489d280f0448e76e67db27bc17
Ka'Niyah Ford <kford2@wvup.edu>
2026-09-06 22:53:40 -0400

    Complete Lesson 1 conversions

---
+++ src/UnitConverter/Pages/Conversions.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UnitConverter.Pages;
+
+public class ConversionsModel : PageModel
+{
+    public string Input { get; set; } = string.Empty;
+    public string Output { get; set; } = string.Empty;
+
+    public void OnGet()
+    {
+        Input = "3.1415";
+
+        ViewData["ConversionType"] = "Miles to Kilometers";
+        ViewData["Title"] = "Conversions";
+
+        double miles = Convert.ToDouble(Input);
+
+        double kilometers = miles * 1.609344;
+
+        Output = kilometers.ToString();
+    }
+}
